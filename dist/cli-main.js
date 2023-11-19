3b8cd7032990327385d1cfc0f6bd7003c9c4a4c6
ocavue <ocavue@gmail.com>
2023-11-20 00:10:31 +0800

    dist

---
+++ dist/cli-main.js
@@ -0,0 +1,8 @@
+"use strict";Object.defineProperty(exports, "__esModule", {value: true});
+
+var _chunk3B72QFV7js = require('./chunk-3B72QFV7.js');
+require('./chunk-UWWD4V4J.js');
+require('./chunk-GQ77QZBO.js');
+
+
+exports.main = _chunk3B72QFV7js.main;
---
+++ dist/cli-node.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+"use strict";
+
+var _chunk3B72QFV7js = require('./chunk-3B72QFV7.js');
+require('./chunk-UWWD4V4J.js');
+
+
+var _chunkUIX4URMVjs = require('./chunk-UIX4URMV.js');
+require('./chunk-GQ77QZBO.js');
+
+// src/cli-node.ts
+_chunk3B72QFV7js.main.call(void 0, {
+  skipNodeModulesBundle: true
+}).catch(_chunkUIX4URMVjs.handleError);
